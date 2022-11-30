917c2945a2eda38de0d1cc261f5914f7844d3647
jeongyunnim <jeongyun7521@gmail.com>
2022-11-30 16:47:42 +0900

    push_swap make deque

--- Makefile
+++ Makefile
@@ -1,1 +1,25 @@
-d
+CC		=	cc
+CFLAGS	=	-Wall -Wextra -Werror -g3 -fsanitize=address
+NAME	=	./push_swap
+INC		=	./push_swap.h
+SRC		=	./push_swap.c
+OBJ		=	$(SRC:%.c=%.o)
+RM		=	rm -f
+
+all		:	$(NAME)
+
+$(NAME)	:	$(OBJ)
+	$(CC) $(CFLAGS) $^ -o $@
+
+$(OBJ)	:	$(SRC) $(INC)
+	$(CC) $(CFLAGS)	-c $^ -I $(INC)
+
+clean	:
+	$(RM) $(OBJ)
+
+fclean	:	clean
+	$(RM) $(NAME)
+
+re		:	fclean all
+
+.PHONY	:	all clean fclean re
--- push_swap.c
+++ push_swap.c
@@ -12,7 +12,7 @@
 
 #include "./push_swap.h"
 
-t_list	*ft_lstnew(int num)
+t_list	*lstnew(int num)
 {
 	t_list	*link;
 
@@ -25,10 +25,24 @@ t_list	*ft_lstnew(int num)
 	return (link);
 }
 
-void	initialize_edge(t_list_edge *edge)
+void	lst_add_back(t_list_edge *edge, int num)
 {
-	edge->head = (t_list *)malloc(sizeof(t_list) * 1);
-	edge->tail = (t_list *)malloc(sizeof(t_list) * 1);
+	t_list	*temp;
+
+	temp = lstnew(num);
+	if (temp == NULL)
+		return ;
+	if (edge->head == NULL)
+	{
+		edge->head = temp;
+		edge->tail = temp;
+	}
+	else
+	{
+		edge->tail->next = temp;//마지막을 가리키던 tail의 next에 현재 만든 노드를 가리키게 한다.
+		temp->previous = edge->tail;//현재 만든 노드를 이전의 tail을 가리키게한다.
+		edge->tail = temp; //tail에 현재 노드를 가리키게 한다.
+	}
 }
 
 int	ft_isspace(char	c)
@@ -65,10 +79,8 @@ int	count_arg(char *data)
 int	split_arg(char **data)
 {
 	int	num;
-	int	i;
 
 	num = 0;
-	i = 0;
 	while (**data != '\0' && ft_isspace(**data) == 0)
 		(*data)++;
 	while (*data != NULL && **data != '\0')
@@ -100,6 +112,26 @@ t_data parse_data(char *data)
 	return (data_set);
 }
 
+void    arr_to_deque(t_data data, t_list_edge *edge)
+{
+	int 	i;
+	t_list	*temp;
+
+	i = -1;
+	while (++i < data.num)
+	{
+		lst_add_back(edge, data.arr[i]);
+	}
+	i = 0;
+	temp = edge->head;
+	while (temp != NULL)
+	{ 
+		printf("%d번째 node: %d\n", i++, temp->num);
+		temp = temp->next;
+	}
+	printf("head: %d | tail: %d\n", edge->head->num, edge->tail->num);
+}
+
 int	main(int argc, char **argv)
 {
 	t_list_edge edge;
@@ -109,15 +141,15 @@ int	main(int argc, char **argv)
 	if (argc < 2)
 		return (write(2, "ARGUMENT COUNT ERROR\n", 21));
 	data = parse_data(argv[1]);
-	initialize_edge(&edge);
 	i = -1;
+	edge.head = NULL;
 	printf("data.num: %d\n", data.num);
 	while (++i < data.num)
 	{
 		printf("arr[%d]: %d\n", i, (data.arr)[i]);
 	}
+    arr_to_deque(data, &edge);
 	free(data.arr);
-
 /*
 	1. 덱 구현하기. - 배열의 요소를 덱에 넣어보기.
 	2. push, reverse, swap 등 명령어 구현하기.
